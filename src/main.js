10e57c6344009640dfcaf4d46b9c3b78837f7187
Georg Lang <georglang0788@gmx.net>
2014-09-19 09:36:53 +0200

    removed getCurrentTime, added msToTime and getMilliSeconds

--- src/main.js
+++ src/main.js
@@ -1,39 +1,51 @@
-function getCurrentTime() {
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var milliseconds = currentTime.getMilliseconds();
 
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-
-  return {
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds,
-    milliseconds: milliseconds
-  }
+function getMilliSeconds(){
+  return new Date().getTime();
 }
 
+function msToTime(duration) {
+  var milliseconds = parseInt((duration%1000)/100)
+      , seconds = parseInt((duration/1000)%60)
+      , minutes = parseInt((duration/(1000*60))%60)
+      , hours = parseInt((duration/(1000*60*60))%24);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+}
+
+function getTimeDifference(startTime, endTime){
+  var timeDifference  = endTime - startTime;
+  return timeDifference;
+}
+
+
 function printCurrentTime(domElement, currentTime) {
-  domElement.innerHTML = currentTime.hours + ":" + currentTime.minutes + " " + ":" + currentTime.seconds + ":" + currentTime.milliseconds;
+    var output = msToTime(currentTime);
+    domElement.innerHTML = output;
 }
 
 
 var elStart1 = document.getElementById("start1");
 var elEnd1 = document.getElementById("end1");
-var diff1 = document.getElementById("diff1");
+var elDiff1 = document.getElementById("diff1");
+var diff1;
 
-
-var start1Time = getCurrentTime();
+var start1Time = getMilliSeconds();
+console.log('Converted:', msToTime(start1Time));
 printCurrentTime(elStart1, start1Time);
 
+
 setTimeout(function () {
-  var end1Time = getCurrentTime();
+  var end1Time = getMilliSeconds();
   printCurrentTime(elEnd1, end1Time);
-}, 10000);
+  diff1 = getTimeDifference(start1Time, end1Time);
+  printCurrentTime(elDiff1, diff1);
+}, 60000);
+
+function foo() {
+
+}
+
