1f0766288ae1f5918994785968c920d16c2c9032
Georg Lang <georglang0788@gmx.net>
2014-09-19 11:24:22 +0200

    used setIntervall to print current time

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-
-function getMilliSeconds(){
+function getMilliSeconds() {
   return new Date().getTime();
 }
 
 function msToTime(duration) {
-  var milliseconds = parseInt((duration%1000)/100)
-      , seconds = parseInt((duration/1000)%60)
-      , minutes = parseInt((duration/(1000*60))%60)
-      , hours = parseInt((duration/(1000*60*60))%24);
+  var milliseconds = parseInt((duration % 1000))
+      , seconds = parseInt((duration / 1000) % 60)
+      , minutes = parseInt((duration / (1000 * 60)) % 60)
+      , hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 
   hours = (hours < 10) ? "0" + hours : hours;
   minutes = (minutes < 10) ? "0" + minutes : minutes;
@@ -16,36 +15,48 @@ function msToTime(duration) {
   return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
 }
 
-function getTimeDifference(startTime, endTime){
-  var timeDifference  = endTime - startTime;
+function getTimeDifference(startTime, endTime) {
+  var timeDifference = endTime - startTime;
   return timeDifference;
 }
 
-
 function printCurrentTime(domElement, currentTime) {
-    var output = msToTime(currentTime);
-    domElement.innerHTML = output;
+  var output = msToTime(currentTime);
+  domElement.innerHTML = output;
 }
-
 
 var elStart1 = document.getElementById("start1");
 var elEnd1 = document.getElementById("end1");
 var elDiff1 = document.getElementById("diff1");
 var diff1;
 
+var output2 = document.getElementById("output2");
+
 var start1Time = getMilliSeconds();
 console.log('Converted:', msToTime(start1Time));
 printCurrentTime(elStart1, start1Time);
-
 
 setTimeout(function () {
   var end1Time = getMilliSeconds();
   printCurrentTime(elEnd1, end1Time);
   diff1 = getTimeDifference(start1Time, end1Time);
   printCurrentTime(elDiff1, diff1);
-}, 60000);
+  clearInterval(secondTimeout);
+}, 4000);
 
-function foo() {
+var counter = 1;
 
-}
+var secondTimeout = setInterval(function () {
+  var innerDiv = document.createElement('div');
+  output2.appendChild(innerDiv);
+  innerDiv.innerHTML = counter + '.' + ' - ' + msToTime(getMilliSeconds());
+  counter += 1;
+  secondTimeout;
+}, 100);
+
+
+
+
+
+
 
