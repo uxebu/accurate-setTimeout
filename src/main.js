5ab8ddab127c4c87dfccfaeb4d33c2ac25bc91fe
Georg Lang <georglang0788@gmx.net>
2014-09-19 08:29:14 +0200

    measuring start and end time setTimeout 6000ms

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
-var currentTime = new Date();
-var hours = currentTime.getHours();
-var minutes = currentTime.getMinutes();
-var seconds = currentTime.getSeconds();
-var milliseconds = currentTime.getMilliseconds();
+function getCurrentTime() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var milliseconds = currentTime.getMilliseconds();
 
-if (minutes < 10) {
-  minutes = "0" + minutes;
-}
-if (seconds < 10) {
-  seconds = "0" + seconds;
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  return hours + ":" + minutes + " " + ":" + seconds + ":" + milliseconds;
 }
 
-document.write("<b>" + hours + ":" + minutes + " " + ":" + seconds + ":" + milliseconds + "</b>")
+var start1 = document.getElementById("start1");
+var end1 = document.getElementById("end1");
+var diff1 = document.getElementById("diff1");
+
+
+start1.innerHTML = getCurrentTime();
+
+setTimeout(function () {
+ end1.innerHTML = getCurrentTime();
+ console.log('Start', start1);
+}, 60000);
